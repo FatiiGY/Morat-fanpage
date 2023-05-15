c87f4022a405c1d53fba3420a9c07d7c46ffbe54
fatii <gore_purdy@hotmail.com>
2023-05-14 22:37:55 -0600

    menu

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './menu.css';
+
+function App() {
+  return (
+    <ul className="menu">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Discografia</a></li>
+    <li><a href="#">Videos</a></li>
+    <li><a href="#">Biografia</a></li>
+  </ul>
+  );
+}
+
+export default App;
